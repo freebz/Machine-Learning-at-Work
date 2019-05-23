13ff3d3c882b6069c9f3e980b4e27a4132c54c06
FreeBz <freebz@hananet.net>
2019-05-23 18:48:33 +0900

    4장 기존 시스템에 머신러닝 통합하기

---
+++ ch04/ex4-1.py
@@ -0,0 +1,2 @@
+sum = w_1 + w_2 + w_3 + ... + w_100
+w_target = sum / 100
---
+++ ch04/ex4-2.py
@@ -0,0 +1,8 @@
+sum = 0
+cnt = 0
+while has_weight():
+    w_tmp = get_wieght()
+    sum += w_temp
+    cnt += 1
+
+w_target = sum / cnt

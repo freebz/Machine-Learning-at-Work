8b0b5b9d87cfc4860f5f975f899091d512fb04d3
freebz <freebz@hananet.net>
2019-05-30 02:10:36 +0900

    7장 프로젝트 1: 영화 추천 시스템 만들기

--- ch07/ex7-14.py
+++ ch07/ex7-14.py
@@ -1,2 +1,6 @@
+r_cols = ['user_id', 'movie_id', 'rating', 'timestamp']
+ratings = pd.read_csv('ml-100k/u.data', sep='\t',
+                      names=r_cols, usecols=range(4), encoding="latin1")
+
 movie_rating = pd.merge(movies, ratings)
 lens = pd.merge(movie_rating, users)
--- ch07/ex7-21.py
+++ ch07/ex7-21.py
@@ -8,7 +8,7 @@ y = np.array([5.0, 1.0, 2.0, 4.0])
 # ALS로 FM 회귀 모델을 초기화한 뒤, 학습을 수행한다.
 fm = als.FMRegression(n_iter=1000, init_stdev=0.1, rank=2,
                       l2_reg_w=0.1, l2_reg_V=0.5)
-fm_fit(X, y)
+fm.fit(X, y)
 # 나이가 24세인 사용자 ID 5가 아이템 10에 대해 내렸을 평점 rate를 예측한다.
 fm.predict(v.transform({"user": "5", "item": "10", "age": 24}))
 # array([ 3.60775939])
